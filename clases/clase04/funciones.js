464652f863541025b23f7f2bd565f238fd77c3ce
sebastian.andrada <sebastian.andrada@icbc.com.ar>
2019-09-18 16:06:05 -0300

    mejora metodo guardar

--- clases/clase04/funciones.js
+++ clases/clase04/funciones.js
@@ -5,10 +5,10 @@ let tabla = document.getElementById('tabla')
 function guardar() {
   let nombre = document.getElementById('nombre')
   let apellido = document.getElementById('apellido')
-  let tabla = document.getElementById('tabla')
+  let tablaRef = document.getElementById('tabla').getElementsByTagName('tbody')[0]
   let form = document.getElementById('agregar')
   verificarVacio()
-  let nuevoRow = `<tr>
+  let nuevoContenidoRow = `<tr>
     <td>${nombre.value}</td>
     <td>${apellido.value}</td>
     <td>
@@ -16,37 +16,42 @@ function guardar() {
     <a href="" onclick="editar(event)">editar</a>
   </td>
   </tr>`
-    tabla.innerHTML += nuevoRow
-    form.hidden = true
+  form.hidden = true
+  let newRow = tablaRef.insertRow()
+  newRow.innerHTML = nuevoContenidoRow
+  nombre.value = ''
+  apellido.value= ''
 }
 
 function verificarVacio() {
   let nombre = document.getElementById('nombre')
   let apellido = document.getElementById('apellido')
-  if (nombre.value == "" || apellido.value == "") {
-    nombre.className += "error"
-    apellido.className += "error"
-    alert("Debe ingresar un nombre y un apellido")
+  if (nombre.value == '' || apellido.value == '') {
+    nombre.className += 'error'
+    apellido.className += 'error'
+    alert('Debe ingresar un nombre y un apellido')
     return
   }
 }
 
 function borrar(e) {
-  //alert("se borro")
   e.preventDefault()
-  e.target.parentNode.parentNode.innerHTML = ""
+  e.target.parentNode.parentNode.innerHTML = ''
 }
 
 function editar(e) {
-  abrir()
+  abrirModificar()
   e.preventDefault()
   borrar(e)
-  //tds = e.target.parentNode.parentNode.children
-
+  tds = e.target.parentNode.parentNode.children
 }
 
 function cerrar() {
+  let nombre = document.getElementById('nombre')
+  let apellido = document.getElementById('apellido')
   let form = document.getElementById('agregar')
+  nombre.value = ''
+  apellido.value = ''
   form.hidden = true
 }
 
