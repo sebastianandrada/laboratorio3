fdf03236bedbaaaab824abe158b0c7f3a9d3da0f
alumno <alumno@utnfra.com.ar>
2019-11-15 22:03:02 -0300

    namespaces

--- clases/clase10/ejemploClase.ts
+++ clases/clase10/ejemploClase.ts
@@ -1,16 +1,18 @@
 namespace animales {
-    var miPerro = new Dog('toby')
-    var miGato = new Cat('gato')
+    export function makeSounds():void {
+        var miPerro:Animal = new Dog('toby')
+        var miGato:Animal = new Cat('gato')
+    
+        var lista: Array<Animal> = new Array<Animal>()
+        lista.push(miPerro)
+        lista.push(miGato)
+    
+        lista.forEach(function (item) {
+            item.makeSound()
+        })
+    }
+    
 
-    var lista: Array<Animal> = new Array<Animal>()
-    lista.push(miPerro)
-    lista.push(miGato)
-
-    lista.forEach(function (item) {
-        item.makeSound()
-    })
-
-    miPerro.makeSound()
 
 }
 
